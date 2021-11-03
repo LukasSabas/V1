1ba869087a8e34b7f686a0af732dacf7386e44c2
LukasSabas <90049977+LukasSabas@users.noreply.github.com>
2021-11-03 16:44:13 +0200

    Add files via upload

---
+++ v0.5/lstudentas.h
@@ -0,0 +1,16 @@
+#ifndef lstudentas_h
+#define lstudentas_h
+#include <iostream>
+#include <list>
+
+struct lstudentas {
+	std::string var;
+	std::string pav;
+	std::list<int> nd = {};
+	double egz;
+	float med;
+	double vid;
+	double galm;
+	double galv;
+};
+#endif
